7b74801989fe186c785f27d81384d84a1641d080
21H41A0529 <lakshmikumpatla25@gmail.com>
2024-07-17 00:45:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [matrix, setMatrix] = useState(Array(3).fill(Array(3).fill('')));
+  const [clicks, setClicks] = useState([]);
+
+  const handleBoxClick = (rowIndex, colIndex) => {
+    if (clicks.length === 8) {
+      const newMatrix = matrix.map((row, i) =>
+        row.map((col, j) => (clicks.some(click => click[0] === i && click[1] === j) ? 'orange' : col))
+      );
+      setMatrix(newMatrix);
+    } else {
+      const newMatrix = matrix.map((row, i) =>
+        row.map((col, j) => (i === rowIndex && j === colIndex ? 'green' : col))
+      );
+      setMatrix(newMatrix);
+      setClicks([...clicks, [rowIndex, colIndex]]);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="matrix">
+      {matrix.map((row, rowIndex) => (
+        <div key={rowIndex} className="row">
+          {row.map((col, colIndex) => (
+            <div
+              key={colIndex}
+              className="box"
+              style={{ backgroundColor: matrix[rowIndex][colIndex] }}
+              onClick={() => handleBoxClick(rowIndex, colIndex)}
+            />
+          ))}
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
